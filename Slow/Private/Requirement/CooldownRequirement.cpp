d57f9a2b7b4b8f76adaedc59fca8f956993b5897
장호철 <wkdghcjf1234@naver.com>
2020-10-06 23:47:51 +0900

    기초 재사용 대기 시간 조건 클래스 추가

---
+++ Slow/Private/Requirement/CooldownRequirement.cpp
@@ -0,0 +1,50 @@
+﻿// Copyright 2020 Team slow. All right reserved.
+
+#include "Requirement/CooldownRequirement.h"
+
+#include "LogDefine.h"
+
+FCooldownRequirement::FCooldownRequirement()
+{
+	BaseCooldownInSeconds = 0;
+	ActiveCooldownInSeconds = 0;
+}
+
+void FCooldownRequirement::SetupCooldown(float InCooldownInSeconds)
+{
+	if (InCooldownInSeconds < 0.0f) {
+		UE_LOG(LogSlow, Warning, TEXT("FCooldownRequirement::SetupCooldown(): 매개 변수로 전달된 시간이 0초보다 작습니다. 즉시 시전을 표현하려면 0을 사용하십시오. 이 값은 0으로 설정됩니다. 설정된 값: %g"), InCooldownInSeconds);
+
+		InCooldownInSeconds = 0;
+	}
+
+	BaseCooldownInSeconds = InCooldownInSeconds;
+	ResetCooldown();
+}
+
+bool FCooldownRequirement::Consume(float InDeltaSeconds)
+{
+	bool bValid;
+
+	ActiveCooldownInSeconds -= InDeltaSeconds;
+	if (ActiveCooldownInSeconds <= 0) {
+		ActiveCooldownInSeconds = 0;
+		bValid = true;
+	}
+	else {
+		bValid = false;
+	}
+	
+	return GetResult(bValid);
+}
+
+bool FCooldownRequirement::IsValid() const
+{
+	bool bValid = ActiveCooldownInSeconds <= 0;
+	return GetResult(bValid);
+}
+
+void FCooldownRequirement::ResetCooldown()
+{
+	ActiveCooldownInSeconds = BaseCooldownInSeconds;
+}
---
+++ Slow/Public/Requirement/CooldownRequirement.h
@@ -0,0 +1,39 @@
+﻿// Copyright 2020 Team slow. All right reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RequirementBase.h"
+
+class FCooldownRequirement : public FRequirementBase
+{
+	float BaseCooldownInSeconds;
+	float ActiveCooldownInSeconds;
+
+public:
+	FCooldownRequirement();
+
+	/// <summary>
+	/// 이 요구 사항의 재사용 대기 시간 값을 전달합니다.
+	/// </summary>
+	/// <param name="InCooldownInSeconds"> 시간을 초 단위로 전달합니다. </param>
+	void SetupCooldown(float InCooldownInSeconds);
+
+	/// <summary>
+	/// 현재 진행 중인 재사용 대기 시간을 소비합니다.
+	/// </summary>
+	/// <param name="InDeltaSeconds"> 소비할 시간 값을 전달합니다. </param>
+	/// <returns> <see cref="IsValid"/> 함수와 같은 반환 값을 가집니다. </returns>
+	bool Consume(float InDeltaSeconds);
+
+	/// <summary>
+	/// 현재 진행 중인 재사용 대기 시간 값이 모두 소비되었는지 검사합니다.
+	/// </summary>
+	/// <returns> 결과가 반환됩니다. </returns>
+	bool IsValid() const;
+
+	/// <summary>
+	/// <see cref="SetupCooldown"/> 함수로 설정 된 재사용 대기 시간 값으로 현재 재사용 대기 시간 값을 다시 설정합니다.
+	/// </summary>
+	void ResetCooldown();
+};
