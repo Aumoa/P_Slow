78690ce9f64fbc94a24fdac40ce9eaec00650535
장호철 <wkdghcjf1234@naver.com>
2020-10-18 19:13:28 +0900

    캐릭터 AnimInstance 가져오기 추가

--- Slow/Private/Actor/SlowCharacter.cpp
+++ Slow/Private/Actor/SlowCharacter.cpp
@@ -1,12 +1,21 @@
-// Copyright 2020 Team slow. All right reserved.
+﻿// Copyright 2020 Team slow. All right reserved.
 
 #include "Actor/SlowCharacter.h"
+
+#include "LogDefine.h"
 
 ASlowCharacter::ASlowCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	bCanAnimErrorLog = true;
+
 	CombatTag.bCanTarget = true;
+}
+
+void ASlowCharacter::BeginPlay()
+{
+	Super::BeginPlay();
 }
 
 void ASlowCharacter::SetCombatTag(const FCombatTag& InCombatTag)
@@ -17,4 +26,38 @@ void ASlowCharacter::SetCombatTag(const FCombatTag& InCombatTag)
 FCombatTag ASlowCharacter::GetCombatTag() const
 {
 	return CombatTag;
+}
+
+USlowAnimInstance* ASlowCharacter::GetAnimInstance() const
+{
+	if (!SlowAnimInstance.IsValid())
+	{
+		QueryAnimInstance();
+		if (bCanAnimErrorLog && !SlowAnimInstance.IsValid())
+		{
+			UE_LOG(LogSlow, Error, TEXT("ASlowCharacter::GetAnimInstance(): 애니메이션 인스턴스를 불러오는데 실패하였습니다. 이 로그는 한 번만 출력됩니다."));
+			bCanAnimErrorLog = false;
+		}
+	}
+
+	return SlowAnimInstance.Get();
+}
+
+void ASlowCharacter::QueryAnimInstance() const
+{
+	USkeletalMeshComponent* SkeletalMesh = Cast<USkeletalMeshComponent>(GetComponentByClass(USkeletalMeshComponent::StaticClass()));
+	if (SkeletalMesh == nullptr)
+	{
+		UE_LOG(LogSlow, Error, TEXT("ASlowCharacter::QueryAnimInstance(): 스켈레탈 메시가 설정되지 않았습니다."));
+		return;
+	}
+
+	UAnimInstance* AnimInstance = SkeletalMesh->GetAnimInstance();
+	if (AnimInstance == nullptr)
+	{
+		UE_LOG(LogSlow, Error, TEXT("ASlowCharacter::QueryAnimInstance(): 스켈레탈 메시에 애니메이션이 설정되지 않았습니다."));
+		return;
+	}
+
+	SlowAnimInstance = Cast<USlowAnimInstance>(AnimInstance);
 }
--- Slow/Public/Actor/SlowCharacter.h
+++ Slow/Public/Actor/SlowCharacter.h
@@ -1,4 +1,4 @@
-// Copyright 2020 Team slow. All right reserved.
+﻿// Copyright 2020 Team slow. All right reserved.
 
 #pragma once
 
@@ -9,10 +9,18 @@
 
 #include "SlowCharacter.generated.h"
 
+class USlowAnimInstance;
+
 UCLASS()
 class SLOW_API ASlowCharacter : public ACharacter
 {
 	GENERATED_BODY()
+
+private:
+	// 이 두 변수는 GetAnimInstance에서 캐시된 데이터를 사용하기 위해
+	// mutable 형식으로 사용합니다.
+	mutable TWeakObjectPtr<USlowAnimInstance> SlowAnimInstance;
+	mutable uint8 bCanAnimErrorLog : 1;
 
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
@@ -21,6 +29,13 @@ protected:
 public:
 	ASlowCharacter();
 
+	void BeginPlay() override;
+
 	void SetCombatTag(const FCombatTag& InCombatTag);
 	FCombatTag GetCombatTag() const;
+
+	USlowAnimInstance* GetAnimInstance() const;
+
+private:
+	void QueryAnimInstance() const;
 };
