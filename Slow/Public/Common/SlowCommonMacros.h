018854d5aa1219f455da75db7e73c0cc2d8fad50
장호철 <wkdghcjf1234@naver.com>
2020-11-01 20:05:09 +0900

    Common 매크로 추가

---
+++ Slow/Public/Common/SlowCommonMacros.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2020 Team slow. All right reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Misc/ScopeLock.h"
+
+template<class T>
+inline const char* __nameof__(const T&, const char* xx)
+{
+	return xx;
+}
+
+#define ScopedLock(x) FScopeLock lock_##x(x.Get())
+#define __FUNCTIONT__ *FString(__FUNCTION__)
+#define nameof(x) *FString(__nameof__(x, #x))
