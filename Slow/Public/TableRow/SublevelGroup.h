8c3989e4cd31b0086fa6cc90e6d0969106555d6b
장호철 <wkdghcjf1234@naver.com>
2020-12-03 00:15:10 +0900

    서브레벨 그룹 데이터테이블 행 추가

---
+++ Slow/Public/TableRow/SublevelGroup.h
@@ -0,0 +1,15 @@
+﻿// Copyright 2020 Aumoa.lib. All right reserved.
+
+#pragma once
+
+#include "SublevelGroup.generated.h"
+
+USTRUCT(BlueprintType)
+struct SLOW_API FSublevelGroup : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+	TArray<FString> SublevelsName;
+};
